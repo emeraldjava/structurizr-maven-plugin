dfb7dfa34f88fd104a7009d4db7e4c880f4c3c7c
emeraldjava <emeraldjava@protonmail.com>
2022-11-24 18:58:59 +0000

    Add basic pom and Mojo

---
+++ src/main/java/io/github/emeraldjava/structurizr/MyMojo.java
@@ -0,0 +1,13 @@
+package io.github.emeraldjava.structurizr;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+public class MyMojo extends AbstractMojo {
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+
+    }
+}
